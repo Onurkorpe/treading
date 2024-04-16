94c39f4a22b30c3797461b8bff04682a0137242c
Onurkorpe <55544290+Onurkorpe@users.noreply.github.com>
2024-04-16 13:48:10 +0300

    add cors and fs module

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 'use strict';
-
+const cors = require('cors');
 const express = require('express');
 const app = express();
+const fs = require('fs');
+
+app.use(cors({
+  origin: 'http://localhost:5173'
+}));
 
 app.get('/', function (req, res) {
 
@@ -14,6 +19,16 @@ app.post('/', function (req, res) {
   res.send('Hemen al');
    console.log('istek geldi');
 
+});
+
+app.get('/api/textfile', (req, res) => {
+  fs.readFile('status.txt', 'utf8', (err, data) => {
+    if (err) {
+      res.status(500).send('Dosya okunamadı.');
+      return;
+    }
+    res.send(data);
+  });
 });
 
 app.listen(80, function () {
