360ddc2987e3553009bfcf11462ec0e5b9057613
Onurkorpe <onurkorpe@outlook.com>
2024-04-20 15:13:37 +0300

    fix  bug 4

--- updateStatu.js
+++ updateStatu.js
@@ -8,7 +8,7 @@ const updateStatus = async function (req, res) {
             console.error(err);
             return;
         }
-        console.log(req)
+        console.log(req.body)
         let jsonData = JSON.parse(data);
         let keyData = req.query.time;
 
