acef845a204b624934b0dcdd5258400dafa88f02
Onurkorpe <onurkorpe@outlook.com>
2024-04-21 16:24:11 +0300

    change req body

--- updateStatu.js
+++ updateStatu.js
@@ -8,22 +8,22 @@ const updateStatus = async function (req, res) {
             console.error(err);
             return;
         }
-        console.log(req.body)
-        let jsonData = JSON.parse(data);
-        let coin = req.body.coin;
-        let time = req.body.time;
-        let value = req.body.value;
+        console.log(req.body.coin)
+         let jsonData = JSON.parse(data);
+         let coin = req.body.coin;
+         let time = req.body.time;
+         let value = req.body.value;
 
-       jsonData.data[coin][time]=value;
-        
-        fs.writeFile('status.txt', JSON.stringify(jsonData, null, 2), 'utf8', (err) => {
-            if (err) {
-                console.error(err);
-                return;
-            }
-            res.send('Veri başarıyla güncellendi.');
-            console.log('Veri başarıyla güncellendi.');
-        });
+        jsonData.data[coin][time]=value;
+      
+         fs.writeFile('status.txt', JSON.stringify(jsonData, null, 2), 'utf8', (err) => {
+             if (err) {
+                 console.error(err);
+                 return;
+             }
+             res.send('Veri başarıyla güncellendi.');
+             console.log('Veri başarıyla güncellendi.');
+         });
     });
    
 };
