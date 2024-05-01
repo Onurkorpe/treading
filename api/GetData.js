568878bdf53f20414e390f40cc7c820c0c6b845b
Onurkorpe <onurkorpe@outlook.com>
2024-05-01 13:48:42 +0300

    test5

--- api/GetData.js
+++ api/GetData.js
@@ -16,21 +16,21 @@ const getData = async function getFuturesBalance(process, coin) {
     availableBalance = (availableBalance / 100) * balancePercent;
     console.log("bakiye: " + availableBalance);
 
-    const lastPrice = await binance.prices(symbol);
-    console.log(`${symbol} son fiyatı: ${lastPrice[symbol]}`);
-    let quantity = (availableBalance / lastPrice[symbol]) * 10;
-    quantity = quantity.toFixed(0);
-    console.log(quantity);
+    // const lastPrice = await binance.prices(symbol);
+    // console.log(`${symbol} son fiyatı: ${lastPrice[symbol]}`);
+    // let quantity = (availableBalance / lastPrice[symbol]) * 10;
+    // quantity = quantity.toFixed(0);
+    // console.log(quantity);
 
-    if (process === "buy") {
-      const response = await binance.futuresMarketBuy(symbol, quantity);
-      console.info(response);
-    } else if (process === "sell") {
-      const response = await binance.futuresMarketSell(symbol, quantity);
-      console.info(response);
-    } else {
-      console.error("Geçersiz işlem tipi");
-    }
+    // if (process === "buy") {
+    //   const response = await binance.futuresMarketBuy(symbol, quantity);
+    //   console.info(response);
+    // } else if (process === "sell") {
+    //   const response = await binance.futuresMarketSell(symbol, quantity);
+    //   console.info(response);
+    // } else {
+    //   console.error("Geçersiz işlem tipi");
+    // }
   } catch (error) {
     console.log(error);
   }
