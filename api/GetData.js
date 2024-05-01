cf0e40b8b766fa235a6ba1cb895eb616490d277e
Onurkorpe <onurkorpe@outlook.com>
2024-05-01 13:37:15 +0300

    test 4

--- api/GetData.js
+++ api/GetData.js
@@ -1,24 +1,39 @@
-const Binance = require('node-binance-api');
+const Binance = require("node-binance-api");
 require("dotenv").config();
-
 
 const binance = new Binance().options({
   APIKEY: process.env.API_KEY,
-  APISECRET: process.env.API_SECRET
+  APISECRET: process.env.API_SECRET,
 });
 
 const getData = async function getFuturesBalance(process, coin) {
+  try {
+    const symbol = coin;
+    console.log(`${symbol}coin `);
+    const balancePercent = 10;
+    const getBalance = await binance.futuresBalance();
+    let availableBalance = getBalance[6].availableBalance;
+    availableBalance = (availableBalance / 100) * balancePercent;
+    console.log("bakiye: " + availableBalance);
 
+    const lastPrice = await binance.prices(symbol);
+    console.log(`${symbol} son fiyatı: ${lastPrice[symbol]}`);
+    let quantity = (availableBalance / lastPrice[symbol]) * 10;
+    quantity = quantity.toFixed(0);
+    console.log(quantity);
 
-  
-try {
-  const data = await binance.futuresBalance();
-  //console.info(data[6].availableBalance );
-  console.info( await binance.futuresMarketBuy( 'CKBUSDT', 900 ) );
-} catch (error) {
-  console.log(error)
-}
-
+    if (process === "buy") {
+      const response = await binance.futuresMarketBuy(symbol, quantity);
+      console.info(response);
+    } else if (process === "sell") {
+      const response = await binance.futuresMarketSell(symbol, quantity);
+      console.info(response);
+    } else {
+      console.error("Geçersiz işlem tipi");
+    }
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 module.exports = getData;
