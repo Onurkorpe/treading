8791287f0e6bbdb1f9a50222e4d47896721d7b9f
Onurkorpe <onurkorpe@outlook.com>
2024-05-02 23:08:25 +0300

    fix position amount

--- api/GetData.js
+++ api/GetData.js
@@ -6,44 +6,52 @@ const binance = new Binance().options({
   APISECRET: process.env.API_SECRET,
 });
 
-const getData = async function getFuturesBalance(process, coin,amount) {
+const getData = async function getFuturesBalance(process, coin, amount) {
   try {
-  
-
     console.log(amount);
-    if (coin === 'PEPEUSDT') {
-      coin = '1000PEPEUSDT';
-      amount = amount *1000;
+    if (coin === "PEPEUSDT") {
+      coin = "1000PEPEUSDT";
+      amount = amount * 1000;
     }
     const symbol = coin;
-    
+
     console.log(`coin ${symbol} `);
 
     const balancePercent = 50;
     const getBalance = await binance.futuresBalance();
-    let crossWalletBalance = getBalance[6].crossWalletBalance;
-    crossWalletBalance = (crossWalletBalance / 100) * balancePercent;
-    crossWalletBalance = crossWalletBalance.toFixed(2);
-      console.log(await binance.futuresBalance());
-      console.log('bakiye: ' + crossWalletBalance);
-      
-      
-     const lastPrice = amount;
-     console.log(`${symbol} son fiyatı: ${lastPrice}`);
-     let quantity = (crossWalletBalance / lastPrice) * 10;
-     quantity = quantity.toFixed(0);
-     console.log("miktar " +quantity);
-
-      if (process === "buy") {
-        const response = await binance.futuresMarketBuy(symbol, quantity);
-        console.info(response);
-      } else if (process === "sell") {
-         const response = await binance.futuresMarketSell(symbol, quantity);
-         console.info(response);
-       } else {
-         console.error("Geçersiz işlem tipi");
+    const positions = await binance.futuresPositionRisk();
+    for (const position of positions) {
+      if (parseFloat(position.positionAmt) > 0) {
+        await binance.futuresMarketSell(
+          position.symbol,
+          Math.abs(parseFloat(position.positionAmt))
+        );
+      } else if (parseFloat(position.positionAmt) < 0) {
+        await binance.futuresMarketBuy(
+          position.symbol,
+          Math.abs(parseFloat(position.positionAmt))
+        );
       }
-     
+    }
+    console.log("Tüm pozisyonlar kapatıldı.");
+
+    const balance = getBalance[6].balance;
+    const fixedBalance = (balance / 100) * balancePercent.toFixed(2);
+
+    const lastPrice = amount;
+    console.log(`${symbol} son fiyatı: ${lastPrice}`);
+    const quantity = ((fixedBalance / lastPrice) * 10).toFixed(0);
+    console.log("miktar " + quantity);
+
+    if (process === "buy") {
+      const response = await binance.futuresMarketBuy(symbol, quantity);
+      console.info(response);
+    } else if (process === "sell") {
+      const response = await binance.futuresMarketSell(symbol, quantity);
+      console.info(response);
+    } else {
+      console.error("Geçersiz işlem tipi");
+    }
   } catch (error) {
     console.log(error);
   }
